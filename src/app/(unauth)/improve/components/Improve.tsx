0070324d3fa7e8d2683f8ce6bf168532e018b0d5
nhanluongoe <nhanlt18@gmail.com>
2024-06-04 17:04:01 +0700

    refactor: use component map to make it scalable

--- src/app/(unauth)/improve/components/Improve.tsx
+++ src/app/(unauth)/improve/components/Improve.tsx
@@ -4,10 +4,15 @@ import FirstTask from './FirstTask';
 import SecondTask from './SecondTask';
 import { useSearchParams } from 'next/navigation';
 
+const taskComponentMapping: Record<string, React.ComponentType> = {
+  task1: FirstTask,
+  task2: SecondTask,
+};
+
 export default function Improve() {
   const searchParams = useSearchParams();
   const task = searchParams.get('task') ?? 'task1';
+  const TaskComponent = taskComponentMapping[task] ?? FirstTask;
 
-  if (task === 'task1') return <FirstTask />;
-  return <SecondTask />;
+  return <TaskComponent />;
 }
