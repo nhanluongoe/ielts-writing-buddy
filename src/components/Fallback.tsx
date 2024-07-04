6bda6a84edd6ac0d141730710136e70dd0e8126f
nhanluongoe <nhanlt18@gmail.com>
2024-07-04 14:15:16 +0700

    chore: update loading color

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -5,8 +5,8 @@ export default function Fallback() {
     <div className="w-screen h-screen">
       <div className="flex items-center justify-center w-full h-full">
         <div className="flex flex-col items-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-          <div className="text-gray-900 text-2xl font-bold mt-4">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-zinc-100"></div>
+          <div className="text-zinc-100 text-2xl font-bold mt-4">
             Loading...
           </div>
         </div>
