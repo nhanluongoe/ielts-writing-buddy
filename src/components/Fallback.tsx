2ddef1eb3275015063012dca974b97a9e4974bc2
nhanluongoe <nhanlt18@gmail.com>
2024-07-26 10:44:00 +0700

    chore: make Fallback screen minimal

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -3,13 +3,8 @@ import React from 'react';
 export default function Fallback() {
   return (
     <div className="w-screen h-screen">
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="flex flex-col items-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-zinc-100"></div>
-          <div className="text-zinc-100 text-2xl font-bold mt-4">
-            Loading...
-          </div>
-        </div>
+      <div className="flex justify-center items-center h-full w-full">
+        <div className="text-5xl font-bold text-gray-400">Loading...</div>
       </div>
     </div>
   );
