c4ebf0cc9668c2342d3790e2c24b51bf63a7c352
nhanluongoe <nhanlt18@gmail.com>
2025-04-05 21:55:21 +0700

    chore: remove unused import

--- src/app/(unauth)/improve/api/first-task/stream/route.ts
+++ src/app/(unauth)/improve/api/first-task/stream/route.ts
@@ -1,4 +1,4 @@
-import { generateContent, generateContentStream } from '@/libs/google-gemini';
+import { generateContentStream } from '@/libs/google-gemini';
 
 const PROMPT = `Based on the provided question, answer, and image for Task 1 in the IELTS Writing exam, write an enhanced response in IELTS style.
 The enhanced response must be no less than 160 words in length.
